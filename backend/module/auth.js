c997f403802d1a814b6c01e94be8c2c91fea5a93
kojiadrianojr <rolando.adriano@boom.camp>
2019-11-19 14:15:28 +0800

    fixed some bugs, restructured console

--- backend/module/auth.js
+++ backend/module/auth.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const jwt = require('jsonwebtoken')
 const userDb = JSON.parse(fs.readFileSync('./backend/json/db.json', 'UTF-8'))
 const expiresIn = '1h'
-
+const SECRET_KEY = process.env.SECRET_KEY
 
 function verifyToken(token){
-    return jwt.verify(token, process.env.SECRET_KEY, (err,decode) => decode != undefined? decode:err)
+    return jwt.verify(token, SECRET_KEY, (err, decoded) => decoded != undefined? decoded:err)
 }
 
 function isAuthenticated({username, password}){
@@ -13,7 +13,7 @@ function isAuthenticated({username, password}){
 }
 
 function createToken(payLoad){
-    return jwt.sign(payLoad, process.env.SECRET_KEY, {expiresIn})
+    return jwt.sign(payLoad, SECRET_KEY, {expiresIn})
 }
 
 
