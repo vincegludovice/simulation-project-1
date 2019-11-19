fddb46657c21406ce9129c10506ddb52fad6aca2
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-11-19 12:36:30 -0700

    Update database init script to add db.json automatically

--- .bin/populateUsers.sh
+++ .bin/populateUsers.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env zsh
 
-./node_modules/.bin/json-server db.json -m ./node_modules/json-server-auth &
+DB_FILE=db.json
+
+if [ -f $DB_FILE ]; then
+  echo "Overwriting existing $DB_FILE"
+  echo '{ "users": [] }' >| $DB_FILE
+else
+  echo '{ "users": [] }' > $DB_FILE
+fi
+
+./node_modules/.bin/json-server $DB_FILE -m ./node_modules/json-server-auth &
 
 SERVER_PID=$!
 
@@ -13,4 +22,6 @@ done
 
 kill "$SERVER_PID"
 
-echo "database seeded"
+echo "OK: database seeded"
+
+exit 0
